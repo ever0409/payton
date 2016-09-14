b6106e4722fe254279f2c5f810765bb376302ff8
ever0409 <sajad0409@gmail.com>
2016-09-15 03:37:10 +0430

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,6 @@ def stats(m):
 
 @bot.message_handler(commands=['mr'])
 def welcome(m):
-    cid = m.chat.id
     markup = types.InlineKeyboardMarkup()
     ret_msg = bot.send_message(cid, "Write Soon", disable_notification=True, reply_markup=markup)
     assert ret_msg.message_id
