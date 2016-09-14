eaa32e30f322c5233714a5d6bba2cf9632e5739c
ever0409 <sajad0409@gmail.com>
2016-09-15 02:09:08 +0430

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,13 @@ bot = telebot.TeleBot(config.token)
 def welcome(m):
     cid = m.chat.id
     markup = types.InlineKeyboardMarkup()
-    b = types.InlineKeyboardButton("MY father",callback_data='@mr_nitto')
+    b = types.InlineKeyboardButton("MY father",callback_data='@mr_nitro')
     markup.add(b)
     nn = types.InlineKeyboardButton("Inline Model", switch_inline_query='')
     markup.add(nn)
     redis = r.StrictRedis(host='localhost', port=6379, db=0)
     redis.sadd('start','{}'.format(m.from_user.id))
-    ret_msg = bot.send_message(cid, "*Hello And \n\n Welcome to GrandexBot \n\n PLease choose one :-D*", disable_notification=True, reply_markup=markup)
+    ret_msg = bot.send_message(cid, "<b>Hello And \n\n Welcome to GrandexBot \n\n PLease choose one :-D</b>", disable_notification=True, reply_markup=markup)
     assert ret_msg.message_id
 
 @bot.callback_query_handler(func=lambda call: True)
